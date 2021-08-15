b4e976ef2f7ba7eac5554a9f2d1ff4720122c6b4
sporeball <sporeballdev@gmail.com>
2021-08-15 12:55:14 -0700

    fish: add my functions into the file

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,25 @@
-set -g fish_prompt_pwd_dir_length 1
+# sporeball's fish config
+
+# Fisher is also used, with the following plugins:
+# - danhper/fish-ssh-agent
+
+set -x SHELL /usr/bin/fish
+
+function cti -d "move an image from the clipboard to a file"
+  xclip -selection clipboard -t image/png -o > $argv
+end
+
+function dcd -d "cd to the contents of a removable volume"
+  cd /run/media/lux
+  cd $argv
+end
+
+function re -d "cd to a repo"
+  cd ~/Documents/gh
+  cd $argv
+end
+
+function td -d "cd to something in the home directory"
+  cd ~
+  cd $argv
+end
